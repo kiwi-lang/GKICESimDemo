e7fd7dac8991be963c460064047992632cf7ac38
Setepenre <pierre.delaunay@outlook.com>
2023-08-20 14:42:19 -0400

    -

--- Source/GKICESimDemo/GKICESimDemo.Build.cs
+++ Source/GKICESimDemo/GKICESimDemo.Build.cs
@@ -7,8 +7,9 @@ public class GKICESimDemo : ModuleRules
 	public GKICESimDemo(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ChaosVehicles", "HeadMountedDisplay", "PhysicsCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ChaosVehicles", "HeadMountedDisplay", "PhysicsCore" });
 
 		PublicDefinitions.Add("HMD_MODULE_INCLUDED=0");
 	}
